b10efeacebf2f0aec42d213ed6406c40e7a0b889
adler-pelzer-dev <adleroee@gmail.com>
2024-06-10 08:24:57 -0300

    update

--- src/domain/entities/Worker/Worker.ts
+++ src/domain/entities/Worker/Worker.ts
@@ -1,20 +1,20 @@
-import { Column, Entity, Generated, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm'
 import { User } from '@/domain/entities/User/User'
 import { WorkerSpecialtys } from '@/domain/entities/Worker/WorkerSpecialtys'
 import { ObjectUtils } from '@/utils/ObjectUtils'
+import { Column, Entity, Generated, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm'
 
-@Entity({name: 'worker'})
+@Entity({ name: 'worker' })
 export class Worker {
 
-    @PrimaryColumn({type: 'nchar', name: 'id'})
+    @PrimaryColumn({ type: 'nchar', name: 'id' })
     @Generated("uuid")
     id: string
 
     @OneToOne(() => User)
-    @JoinColumn({name: 'user_data'})
+    @JoinColumn({ name: 'user_data' })
     userData: User
 
-    @Column('nchar', {name: 'specialty'})
+    @Column('nchar', { name: 'specialty' })
     specialty: WorkerSpecialtys
 
     setSpecialty(value: WorkerSpecialtys) {
@@ -22,13 +22,12 @@ export class Worker {
         return this
     }
 
-
-    setUserData(value: User){
+    setUserData(value: User) {
         this.userData = value
         return this
     }
 
-    static getWorkerSpecialtyValues(){
+    static getWorkerSpecialtyValues() {
         const specialtys = ObjectUtils.listNoNumberProperties(WorkerSpecialtys)
         return specialtys
     }
